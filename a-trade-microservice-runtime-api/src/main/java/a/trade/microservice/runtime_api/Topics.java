d69937444da6b50548e1bb0af89235f6296d3f7d
Adrian <adrian.kuhn@arcor.de>
2025-11-28 23:02:40 +0100

    minr(runtime-api): refactor `Topics` enums and add `getDurationInMillis`
    - rename `TimeWindow` to `TimeWindowSize` and update references for clarity
    - add `Duration` handling to `TimeUnit` and implement `getDurationInMillis` in `Instance`
    - improve enum structure and enhance topic configuration handling

--- a-trade-microservice-runtime-api/src/main/java/a/trade/microservice/runtime_api/Topics.java
+++ a-trade-microservice-runtime-api/src/main/java/a/trade/microservice/runtime_api/Topics.java
@@ -1,5 +1,7 @@
 package a.trade.microservice.runtime_api;
 
+import java.time.Duration;
+
 public class Topics {
 
     private Topics() {}
@@ -9,47 +11,64 @@ public class Topics {
      * Represents predefined instances of various data aggregation and topic
      * configurations.
      * Each {@code Instance} defines specific combinations of {@link ContentType},
-     * {@link AggregateKind}, {@link TimeWindow}, and {@link TimeUnit}, intended to
+     * {@link AggregateKind}, {@link TimeWindowSize}, and {@link TimeUnit}, intended to
      * simplify
      * topic generation and data handling logic.
      * </p>
      */
     public enum Instance {
         STOCKAGGREGATE_ALL_1_MINUTE(ContentType.STOCKAGGREGATE, AggregateKind.ALL,
-                TimeWindow.ONE, TimeUnit.MINUTE),
+                TimeWindowSize.ONE, TimeUnit.MINUTE),
         STOCKAGGREGATE_SINGLE_1_MINUTE(ContentType.STOCKAGGREGATE, AggregateKind.SINGLE
-                , TimeWindow.ONE, TimeUnit.MINUTE),
+                , TimeWindowSize.ONE, TimeUnit.MINUTE),
         ;
 
-        public final ContentType   contentType;
-        public final AggregateKind aggregateKind;
-        public final TimeWindow    timeWindow;
-        public final TimeUnit      timeUnit;
+        public final ContentType    contentType;
+        public final AggregateKind  aggregateKind;
+        public final TimeWindowSize timeWindowSize;
+        public final TimeUnit       timeUnit;
 
         Instance(ContentType contentType, AggregateKind aggregateKind,
-                 TimeWindow timeWindow, TimeUnit timeUnit) {
-            this.contentType   = contentType;
-            this.aggregateKind = aggregateKind;
-            this.timeWindow    = timeWindow;
-            this.timeUnit      = timeUnit;
+                 TimeWindowSize timeWindowSize, TimeUnit timeUnit) {
+            this.contentType    = contentType;
+            this.aggregateKind  = aggregateKind;
+            this.timeWindowSize = timeWindowSize;
+            this.timeUnit       = timeUnit;
         }
 
 
         /**
+         * Computes the duration of the instance's time window in milliseconds.
+         *
+         * <p>
+         * It multiplies the {@link TimeWindowSize} value by the duration of the
+         * associated
+         * {@link TimeUnit}, then converts the result to milliseconds.
+         * </p>
+         *
+         * @return The duration of the time window, in milliseconds.
+         */
+        public long getDurationInMillis() {
+            return timeUnit.getDuration()
+                           .multipliedBy(timeWindowSize.value)
+                           .toMillis();
+        }
+
+        /**
          * Constructs a root name based on the instance's {@link ContentType},
-         * {@link AggregateKind}, {@link TimeWindow}, and {@link TimeUnit}.
+         * {@link AggregateKind}, {@link TimeWindowSize}, and {@link TimeUnit}.
          *
          * @return A {@link String} representing the root name of the aggregate based
          * on the instance's properties.
          */
         public String rootName() {
-            return contentType.value() + "_" + aggregateKind.value() + "_" + timeWindow.value() + "_" + timeUnit.value();
+            return contentType.value() + "_" + aggregateKind.value() + "_" + timeWindowSize.value() + "_" + timeUnit.value();
         }
 
         /**
          * Constructs a unique topic name for the specified ticker based on the current
          * instance's
-         * {@link ContentType}, {@link AggregateKind}, {@link TimeWindow}, and
+         * {@link ContentType}, {@link AggregateKind}, {@link TimeWindowSize}, and
          * {@link TimeUnit}.
          *
          * @param ticker The ticker symbol for which the topic name is to be generated.
@@ -94,12 +113,12 @@ public class Topics {
         }
     }
 
-    public enum TimeWindow {
+    public enum TimeWindowSize {
         ONE(1), TWO(2), FIVE(5), TEN(10), THIRTY(30),
         ;
         private final int value;
 
-        TimeWindow(int value) {
+        TimeWindowSize(int value) {
             this.value = value;
         }
 
@@ -109,16 +128,23 @@ public class Topics {
     }
 
     public enum TimeUnit {
-        MINUTE("min"), HOUR("hour"), DAY("day"), WEEK("week"), MONTH("month"),
+        MINUTE("min", Duration.ofMinutes(1)), HOUR("hour", Duration.ofHours(1)), DAY(
+                "day", Duration.ofDays(1)), WEEK("week", Duration.ofDays(7)),
         ;
-        private final String value;
+        private final String   value;
+        private final Duration duration;
 
-        TimeUnit(String value) {
-            this.value = value;
+        TimeUnit(String value, Duration duration) {
+            this.value    = value;
+            this.duration = duration;
         }
 
         public String value() {
             return value;
+        }
+
+        public Duration getDuration() {
+            return duration;
         }
     }
 }
